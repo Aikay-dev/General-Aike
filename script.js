84b7c7dbe28760dee8d771bef203c2bb59fcb620
GeneralAike <120015643+GeneralAike@users.noreply.github.com>
2023-01-22 00:59:17 +0100

    added links to actual places on nav bar on main website and on contact website

--- script.js
+++ script.js
@@ -19,4 +19,23 @@ hamburger.addEventListener("click", () => {
 
 function about(){
   window.scrollTo(0, 1100);
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
 }
+
+function service(){
+  let target = document.querySelector(".fourthpart");
+  target.scrollIntoView();
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+}
+
+
+  function aboutmove(url) {
+    url = "index.html"
+    var newWindow = window.open(url, "_self");
+    newWindow.onload = function() {
+      var targetElement = newWindow.document.querySelector(".secondpart");
+      targetElement.scrollIntoView();
+    }
+  }
